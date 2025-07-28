7f13be7a677bac491d520e98e67e8001c2a9ea9d
yaksheshgupta <yaksheshgupta59@gmail.com>
2025-07-28 23:20:54 +0530

    min

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -94,7 +94,7 @@ export default function RootLayout({
                 "https://instagram.com/therealfeedapp"
               ],
               "description": "DZDX is an Indian tech company building AI-powered products like The Real Feed, an app that analyzes Indian news for bias, sentiment, and more.",
-              "foundingDate": "2021",
+              "foundingDate": "2025",
               "founders": [{ "@type": "Person", "name": "Rayan Singh Din" }],
               "address": {
                 "@type": "PostalAddress",
@@ -124,6 +124,33 @@ export default function RootLayout({
           }}
         />
 
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '802246002129134');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=802246002129134&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
         {/* Metricool Tracker */}
         <script
           dangerouslySetInnerHTML={{
@@ -143,6 +170,7 @@ export default function RootLayout({
             `,
           }}
         />
+        
       </head>
       <body>
         <div>
